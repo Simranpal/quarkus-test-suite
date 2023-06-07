4e2bdbe11bfb6e4be857c459cbdc004de5975aef
Simranpal <sgill@redhat.com>
2023-06-06 20:50:21 -0700

    add kubeconfig

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-oc login ${OCP_API_URL} --username=${OCP_CRED_USR} --password=${OCP_CRED_PSW} --insecure-skip-tls-verify=true
+oc login ${OCP_API_URL} --username=${OCP_CRED_USR} --password=${OCP_CRED_PSW} --insecure-skip-tls-verify=true --kubeconfig=/tmp/kubeconfig
+export KUBECONFIG=/tmp/kubeconfig
 
 set -eux
 mvn -B -V clean verify -fae \
