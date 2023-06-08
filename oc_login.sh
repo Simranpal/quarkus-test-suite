a051616a7a3d1ecaa441e0d5aa7bb751d0219bc0
Simranpal <sgill@redhat.com>
2023-06-08 11:37:54 -0700

    Move oc login commands

--- oc_login.sh
+++
@@ -1,2 +0,0 @@
-oc login ${OCP_API_URL} --username=${OCP_CRED_USR} --password=${OCP_CRED_PSW} --insecure-skip-tls-verify=true --kubeconfig=/tmp/kubeconfig
-export KUBECONFIG=/tmp/kubeconfig
--- run.sh
+++ run.sh
@@ -2,6 +2,14 @@
 
 export _JAVA_OPTIONS=-Duser.home=$HOME
 
+CONSOLE_URL=$(cat $SHARED_DIR/console.url)
+OCP_API_URL="https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443"
+OCP_CRED_USR="kubeadmin"
+OCP_CRED_PSW="$(cat ${SHARED_DIR}/kubeadmin-password)"
+
+oc login ${OCP_API_URL} --username=${OCP_CRED_USR} --password=${OCP_CRED_PSW} --insecure-skip-tls-verify=true --kubeconfig=/tmp/kubeconfig
+export KUBECONFIG=/tmp/kubeconfig
+
 set -eux
 mvn -B -V clean verify -fae \
     -Dmaven.repo.local=$PWD/local-repo \
