5edc8bed8feb1e5e86d64495105aed72568d8692
Simranpal <sgill@redhat.com>
2023-06-08 14:18:37 -0700

    test adding self-provisioner role

--- .openshift-interop/oc_login.sh
+++ .openshift-interop/oc_login.sh
@@ -1,2 +1,3 @@
 oc login ${OCP_API_URL} --username=${OCP_CRED_USR} --password=${OCP_CRED_PSW} --insecure-skip-tls-verify=true --kubeconfig=/tmp/kubeconfig
 export KUBECONFIG=/tmp/kubeconfig
+oc adm policy add-cluster-role-to-user self-provisioner system:serviceaccount:quarkus-tests:default
